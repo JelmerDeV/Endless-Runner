2a9c77ba1a886d7ecbace8949550b920c3fa6641
KajG <kaaiman99@hotmail.com>
2016-10-11 10:27:35 +0200

    Start menu.
    ik ben zo blij dat dit werkt

---
+++ Assets/Scripts/SpawnerMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerMovement : MonoBehaviour {
+	private float _spawnerSpeed;
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		_spawnerSpeed = GameObject.Find("Player").GetComponent<playerMovement_>().speed;
+		transform.Translate(Vector3.forward * Time.deltaTime * _spawnerSpeed);
+	}
+}
---
+++ Assets/Scripts/TileController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileController : MonoBehaviour {
+
+
+    public GameObject tilePrefab;
+    public float time = 2f;
+    Vector3 forward = new Vector3(0, 0, 113.6f);
+
+
+	void Start ()
+    {
+       
+    }
+	
+    void Timer()
+    {
+        time -= Time.deltaTime;
+        if (time <= 0)
+        {
+            time = 2f;
+            Spawn();
+        }
+    }
+
+
+    void Spawn()
+    {
+        Instantiate(tilePrefab, this.transform.position, Quaternion.identity);
+        this.transform.position += forward;
+    }
+
+
+	void Update ()
+    {
+        Timer();
+        //Debug.Log(time);
+        // 113.6 IS DE AFSTAND TUSSEN DE TILES>>>>>>>>
+        // 96.9 EERSTE Z WAARDE EERSTE TILE NA DE ORGINELE
+	}
+}
