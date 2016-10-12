44e1ed8d090c1a6c8302b1c768d352e9c9de32e0
JelmerDeV <jelmerdevoogt@hotmail.com>
2016-10-12 09:57:45 +0200

    Versie 1.1.3

---
+++ Assets/obstMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class obstMovement : MonoBehaviour {
+	private float speed = 0.2f;
+	// Use this for initialization
+	void Start () {
+		transform.Rotate(Vector3.right * 90);
+		transform.Rotate(Vector3.up * 180);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.up * speed);
+	}
+}
